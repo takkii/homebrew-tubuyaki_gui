846b11cd6a74b0032aae0a8dd498065bb119c8af
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-11-10 17:40:09 +0900

    Update

--- tubuyaki_gui.rb
+++ tubuyaki_gui.rb
@@ -4,7 +4,7 @@ class TubuyakiGui < Formula
   
   homepage "https://github.com/takkii/tubuyaki_gui"
   url "https://github.com/takkii/tubuyaki_gui/archive/1.0.0_00.tar.gz"
-  sha256 "99a655d904d123b5e2e3ceb5fd5c25a15d4de963e487803830fe7c15984ef9a"
+  sha256 "1043d43d9e4ae7c356e909b5b783e32dc11c794c54c7a7519c36b52aa73611bf"
   head "https://github.com/takkii/tubuyaki_gui.git"
   version "1.0.0"
 
